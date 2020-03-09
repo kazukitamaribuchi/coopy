adbd94cc57d67c240a7274ffc70f738dc499f062
vagrant <vagrant@localhost.localdomain>
2020-03-09 14:12:03 +0000

    test

--- userblog/userblog_views.py
+++ userblog/userblog_views.py
@@ -113,7 +113,7 @@ def insertAccessLog(request, *args, **kwargs):
     access_blog_owner = TARGET_BLOG.user
     access_blog_url = TARGET_BLOG.url
 
-    logger.info(request)
+    logger.info(request.META)
 
     if 'favicon.ico' not in page:
         user = request.user
